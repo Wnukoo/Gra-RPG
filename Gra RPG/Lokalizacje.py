fcfc33e568bf99eaff2036c92b0b0460ec2d2ef4
Mateusz Wnuk <mateuszwnuk24@interia.pl>
2025-03-24 17:37:33 +0100

    The End

--- Gra RPG/Lokalizacje.py
+++ Gra RPG/Lokalizacje.py
@@ -22,7 +22,6 @@ class Lokalizacje:
         akcja = input("Wybierz akcję: ") 
         if akcja == "1":
             self.gracz.up_kowal(5, 5)
-  #          self.gracz.wyswietl_statystyki()
         elif akcja == "2":
             print("Do widzenia!")
         print ("---------------------------------")
@@ -33,7 +32,7 @@ class Lokalizacje:
         print("2. Wyjdź")
         akcja = input("Wybierz akcję: ")
         if akcja == "1":
-           self.gracz.up_karczma(100)
+           self.gracz.up_karczma()
         elif akcja == "2":
             print("Do widzenia!")
         print ("---------------------------------")
@@ -44,18 +43,22 @@ class Lokalizacje:
         print("2. Wyjdź")
         akcja = input("Wybierz akcję: ")
         if akcja == "1":
-            print("Wybierz potwora z ktorym chcesz walczyc: ")
-            for potwor in self.potwory:
-                print(potwor)
-            wybor = input("Wybierz potwora: ")
-            if wybor == "1":
-                print("Walczysz z goblinem!")
-            elif wybor == "2":
-                print("Walczysz z orkiem!")
-            elif wybor == "3":
-                print("Walczysz z golemem!")
-            elif wybor == "4":
-                print("Walczysz ze smokiem!")
+            print("Wybierz potwora:")
+            print("1. Goblin")
+            print("2. Ork")
+            print("3. Golem")
+            print("4. Smok")
+            choose = input("Wybierz potwora: ")
+            if choose == "1":
+                self.gracz.walka_Goblin(30, 5)
+            elif choose == "2":
+                self.gracz.walka_Ork(50, 10)
+            elif choose == "3":
+                self.gracz.walka_Golem(75, 15)
+            elif choose == "4":
+                self.gracz.walka_Smok(100, 20)
+            else:
+                print("Nie ma takiego potwora!")
         elif akcja == "2":
             print("Do widzenia!")
         print ("---------------------------------")
--- Gra RPG/Statystyki.py
+++ Gra RPG/Statystyki.py
@@ -1,5 +1,6 @@
 ﻿class Statystyki:
-    def __init__(self, hp=100, atak=10, gold=50):
+
+    def __init__(self, hp=100, atak=10, gold=20):
         self.hp = hp
         self.atak = atak
         self.gold = gold
@@ -22,23 +23,94 @@
             self.atak += 5
             print(f"Zostało Ci {self.gold} złota, atak zostal ulepszony o 5!")
 
-    def up_karczma(self, hp):
-        self.hp == hp
+    def up_karczma(self):
+        self.hp = 100
         print("Odpoczynek!")
         print(f"Twoje HP wynosi {self.hp}!")
 
+    def walka_Goblin(self, hp, zloto):
+        atak_goblin = 15
+        hp_goblin = 30
+        zloto = 5
+        print("Statystyki goblina:")
+        print(f"HP: {hp_goblin}")
+        print(f"Atak: {atak_goblin}")
+        print(f"Zloto: {zloto}")
+        choose = input("Czy chcesz zaatakować goblina? (T/N): ").upper()
+        if choose == "T":
+            if (atak_goblin >= self.hp & hp_goblin >= self.atak ):
+                print("Przegrałeś!")
+                print("Odpocznij w karczmie aby odzyskać siłę!")
+            else:
+                self.hp -= hp
+                self.gold += zloto
+                print(f"Twoje HP wynosi {self.hp}!")
+                print(f"Zdobywasz {zloto} złota!")
+        else:
+            print("Uciekaj tchórzu!")
 
+    def walka_Ork(self, hp, zloto):
+        atak_ork = 30
+        hp_ork = 50
+        zloto = 10
+        print("Statystyki Orka:")
+        print(f"HP: {hp_ork}")
+        print(f"Atak: {atak_ork}")
+        print(f"Zloto: {zloto}")
+        choose = input("Czy chcesz zaatakować Orka? (T/N): ").upper()
+        if choose == "T":
+            if (atak_ork >= self.hp & hp_ork >= self.atak ):
+                print("Przegrałeś!")
+                print("Odpocznij w karczmie aby odzyskać siłę!")
+            else:
+                self.hp -= hp
+                self.gold += zloto
+                print(f"Twoje HP wynosi {self.hp}!")
+                print(f"Zdobywasz {zloto} złota!")
+        else:
+            print("Uciekaj tchórzu!")
 
+    def walka_Golem(self, hp, zloto):
+        atak_golem = 70
+        hp_golem = 85
+        zloto = 20
+        print("Statystyki Orka:")
+        print(f"HP: {hp_golem}")
+        print(f"Atak: {atak_golem}")
+        print(f"Zloto: {zloto}")
+        choose = input("Czy chcesz zaatakować Orka? (T/N): ").upper()
+        if choose == "T":
+            if (atak_golem >= self.hp & hp_golem >= self.atak ):
+                print("Przegrałeś!")
+                print("Odpocznij w karczmie aby odzyskać siłę!")
+            else:
+                self.hp -= hp
+                self.gold += zloto
+                print(f"Twoje HP wynosi {self.hp}!")
+                print(f"Zdobywasz {zloto} złota!")
+        else:
+            print("Uciekaj tchórzu!")
 
-
-
-
-# Wyświetlanie statystyk
-#gracz.wyswietl_statystyki("Gracza")
-#goblin.wyswietl_statystyki("Goblina")
-#ork.wyswietl_statystyki("Orka")
-#smok.wyswietl_statystyki("Smoka")
-
-# Zmiana statystyk (np. po walce)
-#gracz.zmien_statystyki(hp=-10, atak=2, pieniadze=15)
-#print(gracz)  # Teraz HP: 90, Atak: 12, Pieniądze: 65
+    def walka_Smok(self, hp, zloto):
+        atak_smok = 90
+        hp_smok = 150
+        zloto = 100
+        print("Statystki Smoka:")
+        print(f"HP: {hp_smok}")
+        print(f"Atak: {atak_smok}")
+        print(f"Zloto: {zloto}")
+        choose = input("Czy chcesz zaatakować Smoka? (T/N): ").upper()
+        if choose == "T":
+            if (atak_smok >= self.hp & hp_smok >= self.atak ):
+                print("Przegrałeś!")
+                print("Odpocznij w karczmie aby odzyskać siłę!")
+            else:
+                self.hp -= hp
+                self.gold += zloto
+                print("Pokonales bossa!")
+                print(f"Zdobywasz {zloto} złota!")
+                print("Koniec gry")
+                print("Dziekujemy za gre!")
+                print("Autor: Wnukoo")
+        else:
+            print("Uciekaj tchórzu!")
